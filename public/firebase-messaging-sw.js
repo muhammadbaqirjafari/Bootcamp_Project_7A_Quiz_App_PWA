4cffebcb7a69245907b40c71a90b2ab64d7fb300
muhammadbaqirjafari <muhammadbaqirjafari@gmail.com>
2020-09-18 21:08:51 +0500

    Added firebase sw

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+// importScripts('/__/firebase/7.18.0/firebase-app.js');
+// importScripts('/__/firebase/7.18.0/firebase-messaging.js');
+// importScripts('/__/firebase/init.js');
+
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCjEkV75i0-QU6mSXgiCyhdb2jMDxHpjmA",
+    authDomain: "bootcamp-2020-96d01.firebaseapp.com",
+    databaseURL: "https://bootcamp-2020-96d01.firebaseio.com",
+    projectId: "bootcamp-2020-96d01",
+    storageBucket: "bootcamp-2020-96d01.appspot.com",
+    messagingSenderId: "433774093362",
+    appId: "1:433774093362:web:9dacfe586965888023882b",
+    measurementId: "G-FV2K1J9YH8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
